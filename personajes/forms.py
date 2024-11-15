b529e93f25604b64b22d6e8e2bb61c42d50228e1
Flavio Arzola <flavioarzolamontecino@gmail.com>
2024-11-14 21:58:12 -0300

    rama personajes terminada

--- personajes/forms.py
+++ personajes/forms.py
@@ -4,11 +4,10 @@ from .models import Imagen
 class ImagenForm(forms.ModelForm):
     class Meta:
         model = Imagen
-        fields = ['titulo', 'descripcion', 'imagen', 'Raza']
+        fields = ['titulo', 'raza',  'descripcion', 'imagen']
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'raza': forms.Select(attrs={'class': 'form-control'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
             'imagen': forms.FileInput(attrs={'class': 'form-control'}),
-            'Raza': forms.TextInput(attrs={'class': 'form-control'}),
-
         }
